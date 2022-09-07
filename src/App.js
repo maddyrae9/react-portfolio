4c320c7398c22dcae7f403632bc16b6ca8aab912
Madison Mulligan <madisonmulligan97@gmail.com>
2022-09-07 15:10:07 -0400

    Added about component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './App.css';
+import About from './components/About'
+import Nav from './components/Nav';
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+      <Contact></Contact>
+    <main>
+      <Footer></Footer>
+      </main>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import profilePhoto from '../../assets/images/profile.jpg'
+
+const About = () => {
+    return (
+        <section id="About Me">
+            <h2 className="about-title mx auto">About Me</h2>
+            <div className="fact-title-border mx auto"></div>
+            <div className="container">
+                <div className="row">
+                    <div className='col-lg-7 d-flex align-items-center bio'>
+                    </div>
+                </div>
+                <address className="mt-3">
+                    <h5>
+                        Want to contact me?
+                    </h5>
+                    <a href="email:madisonmulligan97@gmail.com">
+                        madisonmulligan97@gmail.com
+                    </a>
+                </address>
+                <img
+                className="profile-pic my-2 mx-auto"
+                src={profilePhoto}
+                alt="profile" />
+            </div>
+            <div className="bio">
+                <h3 className="mt-4">Who am I?</h3>
+                <div className="bio-title-border mx-auto"></div>
+            <p className="container my-4 px-5">
+                I am a hardworker who has been dedicated to learning web development. I got my certificate from University of Central Florida where I learned HTML, CSS, Javascript, React, and databases. I am a full-stack web developer excited to begin my career. I am excited to be apart of a career that I can constantly learn new things to make me a better developer.
+            </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
